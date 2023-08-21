88b0d425fc724fad760ee7faeb11f2b416c7b1c3
zhangli20080808 <791492434@qq.com>
2023-08-21 21:52:21 +0800

    feat: jsx实现

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,1 +1,8 @@
-export {};
+/**
+1. react 入口，打包结果
+ */
+import { jsx } from './src/jsx';
+export default {
+	version: '0.0.0',
+	createElement: jsx
+};
--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -27,4 +27,42 @@ const ReactElement = function (
 	return element;
 };
 
-export const jsx = (type, config) => {};
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		// 属性中存在key，且值存在，转化为 字符串
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		// 不拷贝原型上的属性
+		if (Object.prototype.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+		const maybeChildrenLength = maybeChildren.length;
+		// children 有两种情况  child  [child,child,child]
+		if (maybeChildrenLength) {
+			if (maybeChildrenLength === 1) {
+				props.children = maybeChildren[0];
+			} else {
+				props.children = maybeChildren;
+			}
+		}
+		return ReactElement(type, key, ref, props);
+	}
+};
+
+// 此处表示生产环节和开发环境 用同一个jsx方法，真实的react中是不同的实现，因为在开发环境可以多做一些额外的检查
+export const jsxDev = jsx;
