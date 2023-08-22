35063e2c2948c1c88ac24d3f670d36c170937c2c
zhangli20080808 <791492434@qq.com>
2023-08-22 22:19:51 +0800

    fix: 处理包的路径

--- srcipts/rollup/utils.js
+++ srcipts/rollup/utils.js
@@ -5,9 +5,17 @@ import ts from 'rollup-plugin-typescript2';
 import cjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 
+// 包路径
 const pkgPath = path.resolve(__dirname, '../../packages');
+// 打包产物路径
 const distPath = path.resolve(__dirname, '../../dist/node_modules');
 
+/**
+ * 处理包路径，一般分两种，源码路径，打包后的路径
+ * @param {*} pkgName
+ * @param {*} isDist 当前是否是打包后的路径
+ * @returns
+ */
 export function resolvePkgPath(pkgName, isDist) {
 	if (isDist) {
 		return `${distPath}/${pkgName}`;
@@ -15,6 +23,11 @@ export function resolvePkgPath(pkgName, isDist) {
 	return `${pkgPath}/${pkgName}`;
 }
 
+/**
+ * 处理 package.json包的路径
+ * @param {*} pkgName
+ * @returns
+ */
 export function getPackageJSON(pkgName) {
 	// ...包路径
 	const path = `${resolvePkgPath(pkgName)}/package.json`;
